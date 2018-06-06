c974d9f66a5f943b7a0cc6cbc8e86b2de8852f5d
anhtv08 <anhtv010888@gmail.com>
2018-06-06 19:33:19 +0800

    adding unit testing framework

---
+++ src/test/java/com/training/dta/gs/GSPreparationTest.java
@@ -0,0 +1,47 @@
+package com.training.dta.gs;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class GSPreparationTest {
+
+    @Test
+    public void maxStairCase() {
+    }
+
+    @org.junit.Test
+    public void countArr() {
+    }
+
+    @org.junit.Test
+    public void countSubArray() {
+    }
+
+    @org.junit.Test
+    public void arryProduct() {
+    }
+
+    @org.junit.Test
+    public void mergeString() {
+    }
+
+    @org.junit.Test
+    public void containsChar() {
+    }
+
+    @org.junit.Test
+    public void repeatedChar() {
+    }
+
+    @org.junit.Test
+    public void countStep() {
+    }
+
+    @org.junit.Test
+    public void countStepUtil() {
+    }
+}
